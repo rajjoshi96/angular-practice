9bbb2eb275dae2efac2a7c36f5bea8ed925ac22b
Raj Joshi <rajjoshi@Rajs-MacBook-Pro.local>
2022-01-19 13:54:45 +0800

    sample upstaging commit

--- binding/src/app/app.component.ts
+++ binding/src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'binding';
+  title = 'Innovation Website';
   comp_name = 'KT';
 }
--- binding/src/app/test/test.component.ts
+++ binding/src/app/test/test.component.ts
@@ -2,21 +2,22 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  template: `
-          <h1>Trying it using inline template</h1>
-          <h2>Welcome !! {{name}}</h2>
-          <p> The calculation of 2+2 is {{2+2}}</p>
-          <p>Length of my name is {{name.length}} and the upper case is {{name.toUpperCase()}}</p>
-          <h2>Look at this amazing site {{"link " + siteUrl}}</h2>
-          `,
+  templateUrl: './test.component.html',
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
   public name = "Terminator";
   public siteUrl = window.location.href;
+  public myID = "testID";
+  public isDisabled = true;
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  calculation(){
+    var a = 5;
+    var b = 15;
+    var c = a*b;
+    return c;
+  }
 }
