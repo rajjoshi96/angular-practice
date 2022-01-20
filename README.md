0d9b846627236645e0a91fb42c44b49d7c3e67da
Raj Joshi <rajjoshi@Rajs-MacBook-Pro.local>
2022-01-20 14:42:32 +0800

    Today's lunch progress

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # angular-practice
+
+
+
+
+Topics Learned so far:
+    1. Hello world
+    2. Components
+    3. Interpolation
+    4. Property Binding
+    5. Class Binding
+    6. Style Binding
+    7. Event Binding
+    8. Template Reference
---
+++ binding/src/app/sample/sample.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sample',
+  templateUrl: './sample.component.html',
+  styleUrls: ['./sample.component.css']
+})
+export class SampleComponent implements OnInit {
+  public course_name = "Angular";
+  public successClass = "text-success";
+  public hasError = true;
+  public Highlight_color="crimson";
+  public greeting="";
+  public titleStyles={
+    color:"blue",
+    fontStyle:"strong",
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  onClick(){
+    console.log("Learning Angular..😪");
+    this.greeting="Whoopee";
+  }
+
+  onlog(value: any){
+    console.log(value);
+  }
+}
